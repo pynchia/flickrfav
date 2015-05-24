d1c676bc51054337386844c36f8607b41f14a4c2
MBB <pynchia@gmail.com>
2015-05-24 22:45:10 +0200

    script to launch it and type less

---
+++ go.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# launch flickrfav
+#SRCPATH='Favorites/'
+SRCPATH='/run/user/1000/gvfs/smb-share:server=tempest,share=flickr/Favorites/'
+DSTPATH=$SRCPATH'Download/'
+echo $SRCPATH
+echo $DSTPATH
+./flickrfav.py 63662636@N07 z $SRCPATH $DSTPATH > out.txt
